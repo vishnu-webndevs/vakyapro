94d15776195bfe2c8aa1389c9aeaa55736bcb44a
webndevs <webndevs@gmail.com>
2026-03-12 11:37:11 +0530

    Add CORS config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\EnsureSuperAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->append(HandleCors::class);
         $middleware->alias([
             'admin.super' => EnsureSuperAdmin::class,
         ]);
---
+++ config/cors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://vakyapro.com',
+        'https://www.vakyapro.com',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
