1e9ad7ad3d99ef752b7117c7f9af0e0ec9ce53a3
webndevs <webndevs@gmail.com>
2026-03-24 16:52:15 +0530

    fix(cors): avoid duplicate Access-Control-Allow-Origin header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use App\Http\Middleware\EnsureUserNotBlocked;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->prepend(HandleCors::class);
         $middleware->alias([
             'admin.super' => EnsureSuperAdmin::class,
             'user.active' => EnsureUserNotBlocked::class,
--- config/cors.php
+++ config/cors.php
@@ -9,9 +9,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
     ],
-    'allowed_origins_patterns' => [
-        '#^https://(www\.)?vakyapro\.com$#',
-    ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
